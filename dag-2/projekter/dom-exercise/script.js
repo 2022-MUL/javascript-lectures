dad02808818324ae6592f18436d7f2e4e373ce44
cph-mtnl <mtnl@cphbusiness.dk>
2022-03-16 11:34:17 +0100

    lightbox

--- dag-2/projekter/dom-exercise/script.js
+++ dag-2/projekter/dom-exercise/script.js
@@ -1,5 +1,27 @@
 (function () {
   window.addEventListener("load", () => {
-    console.log("Index.js was invoked");
+    document.getElementById("button").addEventListener("click", clicked);
+
+    console.log("Script.js was invoked");
+    const h1Element = document.getElementById("title");
+    console.log(h1Element.innerText);
+    h1Element.innerText = "Her er en ny titel";
+
+    const h3Elements = document.getElementsByTagName("h3");
+    console.log(h3Elements);
+
+    const specificH3 = document.querySelector(".container h3");
+    console.log(specificH3.innerText);
+
+    const title = document.getElementById("title");
+    title.addEventListener("click", function () {
+      title.classList.add("a-random-class");
+    });
   });
 })();
+
+function clicked() {
+  const pElement = document.querySelector(".a-random-class");
+  pElement.style.color = "red";
+  pElement.classList.add();
+}
