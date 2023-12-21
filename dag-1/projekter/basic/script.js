62cafc55987099abff6298ba112879231b0f8f18
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2023-12-21 18:05:58 +0100

    some changes

--- dag-1/projekter/basic/script.js
+++ dag-1/projekter/basic/script.js
@@ -1,4 +1,26 @@
-function sendEmail() {
-  console.log("Klikket på")
+// async function call() {
+//   let result = await fetch('https://api.jsonbin.io/v3/qs/62fe1b1c5c146d63ca746eda')
+//   result = await result.json();
+//   console.log(result)
+  
+//   result.record.forEach((dataObj) => {
+//     fetch('https://mul3sem-default-rtdb.europe-west1.firebasedatabase.app/transactions.json', {
+//       method: "POST",
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(dataObj),
+//     })
+//   })
+// }
 
+async function getData() {
+  let result = await fetch('https://mul3sem-default-rtdb.europe-west1.firebasedatabase.app/transactions.json')
+  result = await result.json();
+  console.log(result)
+  const array = []
+  for (let key in result) {
+    array.push(result[key])
+  }
+  console.log(array)
 }
